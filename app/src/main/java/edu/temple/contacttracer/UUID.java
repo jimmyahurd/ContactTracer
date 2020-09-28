0a5c3663eb4f6613f9a1ad15f464991583fa01ae
James <tuh01663@temple.edu.com>
2020-09-28 12:08:02 -0400

    Update UUID.java
    Small fix to logic of evaluating how old UUID's are

--- app/src/main/java/edu/temple/contacttracer/UUID.java
+++ app/src/main/java/edu/temple/contacttracer/UUID.java
@@ -23,11 +23,11 @@ public class UUID{
     }
 
     public boolean olderThan14Days(){
-        return created < System.currentTimeMillis()*14*24*60*60*1000;
+        return created < (System.currentTimeMillis() - 14*24*60*60*1000);
     }
 
     public boolean youngerThan1Day(){
-        return created > System.currentTimeMillis()*24*60*60*1000;
+        return created > (System.currentTimeMillis() - 1*24*60*60*1000);
     }
 
     public JSONObject toJSON() throws JSONException {
